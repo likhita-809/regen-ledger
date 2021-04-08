71f68ab3095dce3a211a27f1e3f52eadc9ef8144
goreleaserbot <goreleaser@carlosbecker.com>
2021-04-08 07:28:21 +0000

    Brew formula update for regen-ledger version v0.8.0

--- regen-ledger.rb
+++ regen-ledger.rb
@@ -5,16 +5,16 @@
 class RegenLedger < Formula
   desc "Blockchain-powered marketplace for climate solutions and planetary regenerations"
   homepage "https://www.regen.network/"
-  version "0.7.9"
+  version "0.8.0"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/likhita-809/regen-ledger/releases/download/v0.7.9/regen-ledger_0.7.9_darwin_amd64.zip"
-    sha256 "874d696be9cffa289fab7e582edbd901f559a4cae55be819c4610b7fb5622051"
+    url "https://github.com/likhita-809/regen-ledger/releases/download/v0.8.0/regen-ledger_0.8.0_darwin_amd64.zip"
+    sha256 "f5c795fe79ae34abd2393814a8891ac1cee4940317301d8a3bb20b64405d5e40"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/likhita-809/regen-ledger/releases/download/v0.7.9/regen-ledger_0.7.9_linux_amd64.zip"
-    sha256 "4209871c9177b5e31eb286add4f7d155cb60f200851ae8595d07951e6fb55421"
+    url "https://github.com/likhita-809/regen-ledger/releases/download/v0.8.0/regen-ledger_0.8.0_linux_amd64.zip"
+    sha256 "b4441d79b8b32ae324365793512c864557643b3e2d041b3c1a9782b6ba41c7cc"
   end
 
   head "https://github.com/likhita-809/regen-ledger.git"
